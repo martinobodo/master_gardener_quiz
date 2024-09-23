73f1c58a400f346059fe7161e0263d1f62c670be
martin obodo <martinobodo99@gmail.com>
2024-09-23 15:16:05 +0000

    created questions.py file

---
+++ questions.py
@@ -0,0 +1,47 @@
+{
+    'category': 'Gardening', 
+    'question': 'What is a rake used for?', 
+    'options': {
+        'A': 'Planting seeds', 
+        'B': 'Leveling soil', 
+        'C': 'Pruning shrubs', 
+        'D': 'Watering plants'
+    },
+    'correct_answer': 'B'
+}
+
+{
+    'category': 'Gardening', 
+    'question': 'What is a trowel used for?', 
+    'options': {
+        'A': 'Pruning shrubs', 
+        'B': 'Watering plants', 
+        'C': 'Digging small holes', 
+        'D': 'Sweeping leaves'
+    },
+    'correct_answer': 'C'
+}
+
+{
+    'category': 'Gardening', 
+    'question': 'What is a hoe used for?', 
+    'options': {
+        'A': 'Breaking up soil', 
+        'B': 'Watering plants', 
+        'C': 'Trimming hedges', 
+        'D': 'Mowing grass'
+    },
+    'correct_answer': 'A'
+}
+
+{
+    'category': 'Gardening', 
+    'question': 'What is a watering can used for?', 
+    'options': {
+        'A': 'Planting seeds', 
+        'B': 'Watering plants', 
+        'C': 'Digging holes', 
+        'D': 'Trimming branches'
+    },
+    'correct_answer': 'B'
+}
