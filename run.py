dc7eb59241036a437151f0e0e11266f8d3103675
martin obodo <martinobodo99@gmail.com>
2024-09-23 15:05:55 +0000

    initial commit

---
+++ run.py
@@ -0,0 +1,10 @@
+"""
+This module contains the logic for running an interactive quiz game.
+It includes functions for managing a leaderboard and displaying questions.
+"""
+
+import random
+import os
+import pyfiglet
+from termcolor import colored
+from questions import QUESTIONS
