d82abb4550d68f71187cc7ca4409f16cf60dc7cc
martin obodo <martinobodo99@gmail.com>
2024-09-23 21:56:31 +0000

    added function def clear

--- run.py
+++ run.py
@@ -11,3 +11,10 @@ from questions import QUESTIONS
 # Assuming QUESTIONS is imported from questions.py
 
 QUIZ_LENGTHS = [10, 20, 50, 100]
+
+
+def clear():
+    """
+    Clear function to clean-up the terminal so things don't get messy.
+    """
+    os.system("cls" if os.name == "nt" else "clear")
